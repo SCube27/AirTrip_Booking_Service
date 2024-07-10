8d75dc7c08c841d4de03a27447be7c1736e5cc93
Sahil Shah <sahilshah2104@gmail.com>
2024-07-10 18:55:30 +0530

    Feat: Completed Booking API, integrated along with transactions

--- src/repositories/bookingRepository.js
+++ src/repositories/bookingRepository.js
@@ -2,10 +2,22 @@ const { StatusCodes } = require('http-status-codes');
 
 const { Booking } = require('../models/index');
 const CrudRepository = require('./crudRepository');
+const { InternalServerError } = require('../errors');
+const { Logger } = require('../config');
 
 class BookingRepository extends CrudRepository {
     constructor() {
         super(Booking);
+    }
+
+    async createBooking(data, transaction) {
+        try {
+            const response = await Booking.create(data, {transaction: transaction});
+            return response;
+        } catch (error) {
+            Logger.error('Some internal problem occured, booking not created!');
+            throw new InternalServerError(`Some internal problem occured can't create a booking`);
+        }
     }
 }
 
--- src/services/bookingService.js
+++ src/services/bookingService.js
@@ -6,22 +6,39 @@ const { InternalServerError, BadRequestError } = require('../errors/index');
 const { ServerConfig } = require('../config/index');
 
 class BookingService { 
-    constructor(bookingService) {
-        this.bookingService = bookingService;
+    constructor(bookingRepository) {
+        this.bookingRepository = bookingRepository;
     }
 
     async createBooking(data) {
-        return new Promise((resolve, reject) => {
-            const result = db.sequelize.transaction(async function bookingImpl(t) {
-                const flight = await axios.get(`${ServerConfig.FLIGHTS_SERVICE}/api/v1/flights/${data.flightId}`);  
-                const flightData = flight.data.data;
-                if(data.noOfSeats > flightData.totalSeats) {
-                    Logger.error("Booking failed due to attempt of booking more seats than remaining.")
-                    reject(new BadRequestError('noOfSeats', "No of seats booked should be less than remaining seats"));
-                }
-                resolve(true);
+        const transaction = await db.sequelize.transaction();
+        try {
+            const flight = await axios.get(`${ServerConfig.FLIGHTS_SERVICE}/api/v1/flights/${data.flightId}`);  
+            
+            const flightData = flight.data.data;
+
+            // checking for the seats requirement
+            if(data.noOfSeats > flightData.totalSeats) {
+                Logger.error("Booking failed due to attempt of booking more seats than remaining.")
+                throw new BadRequestError('noOfSeats', "No of seats booked should be less than remaining seats")
+            }
+
+            // calculating the billing amount for the number of seats booked
+            const totalBillingAmount = data.noOfSeats * flightData.price;
+            
+            const bookingPayload = { ...data, totalCost: totalBillingAmount };
+            const booking = await this.bookingRepository.createBooking(bookingPayload, transaction);
+
+            await axios.patch(`${ServerConfig.FLIGHTS_SERVICE}/api/v1/flights/${data.flightId}/seats`, {
+                seats: data.noOfSeats
             });
-        });
+
+            await transaction.commit();
+            return booking;
+        } catch (error) {
+            await transaction.rollback();
+            throw error;
+        }
     }
 }
 
